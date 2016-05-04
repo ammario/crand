13a53c13e12b8c28ee866a152abdadbd2329d654
Ammar Bandukwala <ammar@ammar.io>
2016-05-03 20:25:05 -0500

    Clarify parameter on String()

--- strings.go
+++ strings.go
@@ -8,9 +8,9 @@ const charSet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 const charSetLen = 62
 
 //String returns a random string using the charset a-z A-Z 0-9
-func String(n int) string {
+func String(size int) string {
 	buf := new(bytes.Buffer)
-	for i := 0; i < n; i++ {
+	for i := 0; i < size; i++ {
 		buf.WriteByte(charSet[Uint(charSetLen)])
 	}
 	return buf.String()
